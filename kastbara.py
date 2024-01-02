bf5e9fa8210f411af3fb2dcf135ccb7976c0192e
ArvindTHEkatare <arvindkatare911@gmail.com>
2024-01-02 15:06:05 +0100

    potential idea, kastbara föremål

---
+++ kastbara.py
@@ -0,0 +1,17 @@
+#import modul
+import random
+import time
+import sys
+
+def forsenadPrint(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+def molotov():
+    
+
+def flash():
+
+
