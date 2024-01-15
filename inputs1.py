9d2fa9f6f01968945ed629c99a04a1e45357e337
ArvindTHEkatare <arvindkatare911@gmail.com>
2024-01-15 10:59:06 +0100

    skev stats för garmit boss

--- inputs1.py
+++ inputs1.py
@@ -43,10 +43,43 @@ def plot():
         else:
             forsenadPrint("Felaktig inmatning, försök igen...\a")
 
-def garmit_info():
+def mc():
+    hp_mc = 50
+    attack_mc = 4
+    turn_mc = 0 
+
     while True:
-        hp = int("100")
-        attack = int("")
+        turn_mc = turn_mc + 1
+        print(f"Turn {turn_mc}")
+
+              
+
+
+    
+#funktionen fär boss1 HP och attacks
+def garmit_info():
+    hp_g = 100
+    attack_g = 5
+    turn = 0
+
+    while True:
+        turn = turn + 1
+        print(f"Turn {turn}")
+
+        hp_g -= attack_g
+
+        print(f"HP: {hp_g}, Attack: {attack_g}")
+
+        if hp_g <= 0:
+            print("Game Over - Garmit defeated!")
+            break
+
+        input("Press Enter to continue to the next turn.")
+            
+
+
+garmit_info()
+
 
 
 forsenadPrint("Välkommen1\n")
